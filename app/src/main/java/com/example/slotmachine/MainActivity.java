181e7d2fb386606f1168fde6182fae1f87070088
f770510 <f770510@gmail.com>
2019-07-14 00:04:29 +0800

    加入logo跳動動畫

--- app/src/main/java/com/example/slotmachine/MainActivity.java
+++ app/src/main/java/com/example/slotmachine/MainActivity.java
@@ -2,19 +2,30 @@ package com.example.slotmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_Start;
+    TextView tv_Logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_Start=(Button)findViewById(R.id.btn_Start);
+        tv_Logo=(TextView)findViewById(R.id.tv_Logo);
         btn_Start.setOnClickListener(btn_StartOnClickListener);
+        ObjectAnimator animTxtFalling =
+                ObjectAnimator.ofFloat(tv_Logo, "y", 0, 500);
+        animTxtFalling.setDuration(2000);
+        animTxtFalling.setRepeatCount(0);
+        animTxtFalling.setInterpolator(new BounceInterpolator());
+        animTxtFalling.start();
     }
     View.OnClickListener btn_StartOnClickListener =new View.OnClickListener(){
         @Override
